1e8bf1c9eeb9f24998b336a03cc30110447ed86c
unknown <josheela.xworkz@gmail.com>
2022-09-28 09:20:34 +0530

    Validation

---
+++ color.java
@@ -0,0 +1,48 @@
+class color{
+	static String[]allColors={null,null,null,null,null,null,null};
+	static int position=0;
+	static void save(String colors)
+	{
+		if(position>=7)
+		{
+			System.out.println("main ");
+			return;
+		}
+		if(allColors!=null && colors.length()>=3)
+		{
+			System.out.println("start");
+	        allColors[position]=colors;
+	        System.out.println("position"+position+"=:"+colors);
+            position++;	
+		}
+	}
+	static void desply()
+	{
+		for(int point=0;point<allColors.length;point++)
+		{
+			String index=allColors[point];
+			System.out.println(index);
+		}
+	}
+	static boolean checkColor(String colors)
+	{
+		if(allColors!=null && colors.length()>=3)
+		{
+			for(int info=0;info<allColors.length;info++)
+			{
+				System.out.println("color is in the memory");
+				String ref=allColors[info];
+				if(ref==colors)
+				{
+					System.out.println("found the color:"+colors);
+					return true;
+				}
+			}
+		}       
+			else
+		         {
+			      System.out.println("color not in memory");
+		         }
+				 return false;
+	}	
+}
---
+++ colorRunner.java
@@ -0,0 +1,19 @@
+class colorRunner
+{
+       public static void main(String[]rudra)
+	   {
+		   System.out.println("Print started");
+		   color.save("RED");
+		   color.save("Orange");
+		   color.save("Yellow");
+		   color.save("White");
+		   color.save("Green");
+		   color.save("BLACK");
+		   color.save("Blue");
+		   color.desply();
+		  boolean ref= color.checkColor("White");
+	   }
+ 	
+	
+	
+}
